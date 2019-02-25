24c7ac96dc9bce654e88a618da688c11f608de07
JHNUXER <johnhoganuke2@gmail.com>
2019-02-25 15:39:03 -0500

    Added restaurant index view

--- RestaurantRater.WebMVC/Models/Restaurant.cs
+++ RestaurantRater.WebMVC/Models/Restaurant.cs
@@ -9,7 +9,7 @@ namespace RestaurantRater.WebMVC.Models
     public class Restaurant
     {
         public int RestaurantID { get; set; }
-        string Name             { get; set; }
+        public string Name             { get; set; }
         public int Rating       { get; set; }
         public string Address   { get; set; }
 
